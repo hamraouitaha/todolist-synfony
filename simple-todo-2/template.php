3afbfcb270187466b21293689c639a54e76785e2
taha <taha.toutou97@gmail.com>
2019-05-10 11:53:18 +0100

    simple-todo-2

---
+++ simple-todo-2/template.php
@@ -0,0 +1,22 @@
+<!-- template.php -->
+<!DOCTYPE html>
+<html>
+<head>
+  <title>List of todos</title>
+</head>
+<body>
+  <h1>List of todos</h1>
+  <ul>
+    <?php foreach ($todos as $todo): ?>
+    <li>
+        <?php
+            if ($todo['is_complete'] == 1)
+                echo '<del>'.$todo['todo'].'</del>';
+            else
+                echo $todo['todo'];
+        ?>
+    </li>
+    <?php endforeach ?>
+  </ul>
+</body>
+</html>
