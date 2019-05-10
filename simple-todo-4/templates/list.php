967f8abde3e4148276b7c157814ba0637022a3c8
taha <taha.toutou97@gmail.com>
2019-05-10 12:09:21 +0100

    simple-todo4

---
+++ simple-todo-4/templates/list.php
@@ -0,0 +1,17 @@
+<!-- template/list.php -->
+<!DOCTYPE html>
+<html>
+<head>
+  <title>List of todos</title>
+</head>
+<body>
+  <h1>List of todos</h1>
+  <ul>
+    <?php foreach ($todos as $todo): ?>
+    <li>
+        <a href="show.php?id=<?= $todo['tid'] ?>">Todo id <?= $todo['tid'] ?>:<?= $todo['todo'] ?></a>
+    </li>
+    <?php endforeach ?>
+  </ul>
+</body>
+</html>
